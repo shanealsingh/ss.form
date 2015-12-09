118d99b2ee952af2015dbd67bafa49f81ef6e8a0
Shaneal Singh <shanealsingh@gmail.com>
2015-12-08 19:39:02 -0800

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
   connect = require('gulp-connect');
  
 gulp.task('connect', function() {
@@ -20,8 +22,24 @@ gulp.task('demo-js', function () {
 });
 
 gulp.task('src-js', function () {
-  gulp.src('./src/*.js')
-    .pipe(connect.reload());
+  var files = gulp.src('./src/*.js');
+
+  var stream = files
+                 .pipe(concat('ss-form.js'))
+                 .pipe(gulp.dest('./demo'))
+                 .pipe(gulp.dest('./dist'));
+
+  
+    files
+     .pipe(concat('ss-form.min.js'))
+     .pipe(uglify())
+     .pipe(gulp.dest('./dist'));
+
+  stream.on('end', function() {
+    if(connect) {
+      connect.reload();
+    }
+  });
 });
 
 gulp.task('watch', function () {
@@ -30,4 +48,4 @@ gulp.task('watch', function () {
   gulp.watch(['./src/*.js'], ['src-js']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['src-js', 'connect', 'watch']);
